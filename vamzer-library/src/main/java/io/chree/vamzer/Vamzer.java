b6a06c19ef4a0eb33ec38666991677f33d8a77cf
Attila Bagossy <bagossyattila@outlook.com>
2020-11-15 23:26:53 +0100

    feat: created the entry point of the library

---
+++ vamzer-library/src/main/java/io/chree/vamzer/Vamzer.java
@@ -0,0 +1,11 @@
+package io.chree.vamzer;
+
+/**
+ * The global entry point of the Vamzer library.
+ */
+public final class Vamzer {
+    /**
+     * This class is not instantiatable.
+     */
+    private Vamzer() {}
+}
