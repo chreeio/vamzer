3a7243ddee4664aa3143e296d815f96d5a3eb046
Attila Bagossy <bagossyattila@outlook.com>
2020-11-16 01:04:17 +0100

    feat: added Vamzer.Checkers

--- vamzer-library/src/main/java/io/chree/vamzer/Vamzer.java
+++ vamzer-library/src/main/java/io/chree/vamzer/Vamzer.java
@@ -1,9 +1,39 @@
 package io.chree.vamzer;
 
+import io.chree.vamzer.checker.SecretChecker;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
 /**
  * The global entry point of the Vamzer library.
  */
 public final class Vamzer {
+    /**
+     * Entry point to easily discover and create new instances for the
+     * secret checkers offered by Vamzer.
+     */
+    public static final class Checkers {
+        /**
+         * Creates a list containing a new, default-configured
+         * ({@link io.chree.vamzer.checker.SecretCheckerFactory#createDefault()}) instance
+         * for every checker type contained in Vamzer.
+         *
+         * The order of checkers in the returned list is pre-configured to make
+         * optimal use of hints.
+         * @return a list of new, default-configured checkers
+         */
+        public static List<SecretChecker> all() {
+            return emptyList();
+        }
+
+        /**
+         * This class is not instantiatable.
+         */
+        private Checkers() {}
+    }
+
     /**
      * This class is not instantiatable.
      */
